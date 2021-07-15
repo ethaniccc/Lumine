61ad5c9b24a25516291daa5cc310442df9db18d0
ethaniccc <schooldocumentsandstuff@gmail.com>
2021-07-15 19:56:02 -0400

    remove plugin files for server branch

--- src/ethaniccc/Lumine/events/HeartbeatEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ethaniccc\Lumine\events;
-
-final class HeartbeatEvent extends SocketEvent{
-
-	public const NAME = self::HEARTBEAT;
-
-}
--- src/ethaniccc/Lumine/events/SendErrorEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ethaniccc\Lumine\events;
-
-final class SendErrorEvent extends SocketEvent {
-
-	public const NAME = self::SOCKET_SEND_ERROR;
-
-}
