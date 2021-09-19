0fda743b512521e6ef55780ad2c1aa630b83939a
Jviguy <jviguytwo2@gmail.com>
2021-09-19 15:27:21 -0400

    working listener fully async.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,18 @@
 use crate::server::settings::Settings;
 use crate::Result;
-use tokio::io::{stdin, BufReader, AsyncBufReadExt};
+use tokio::io::{stdin, BufReader, AsyncBufReadExt, AsyncWriteExt, AsyncReadExt};
 use tokio::sync::mpsc::channel;
 use tokio::task::JoinHandle;
-
+use tokio::net::{TcpListener, TcpStream};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::borrow::BorrowMut;
+use tokio::prelude::*;
 
 mod settings;
+mod events;
 
 pub async fn new() -> Result<Server> {
     let mut path = std::env::current_dir()?;
@@ -14,7 +21,9 @@ pub async fn new() -> Result<Server> {
     Ok(Server {
         settings,
         running: false,
-        threads: vec![]
+        tick: 0,
+        threads: vec![],
+        clients: Arc::new(Default::default())
     })
 }
 
@@ -23,13 +32,15 @@ enum Receivable {
     Event(Event),
 }
 
-enum Event {
-}
+enum Event{}
+
 
 pub struct Server {
     pub settings: Settings,
-    running: bool,
     threads: Vec<JoinHandle<()>>,
+    running: bool,
+    tick: u128,
+    clients: Arc<Mutex<HashMap<SocketAddr, tokio::io::WriteHalf<TcpStream>>>>
 }
 
 impl Server {
@@ -43,7 +54,32 @@ impl Server {
                 tx.send(Receivable::Command(inp.trim().to_string())).await;
             }
         }));
+        let mut listener = TcpListener::bind(self.settings.bind_address.clone() + ":" + &self.settings.server_port.to_string()).await?;
+        let clients_recv = Arc::clone(&self.clients);
+        self.threads.push(tokio::spawn(async move {
+            loop {
+                let (mut sock, addr) = listener.accept().await.unwrap();
+                let (mut reader, mut writer) = tokio::io::split(sock);
+                let mut tmp = clients_recv.lock().await;
+                tmp.insert(addr, writer);
+                tokio::spawn(async move {
+                    let mut buf = vec![0; 1024];
+                    loop {
+                        let n = reader
+                            .read(&mut buf)
+                            .await
+                            .expect("failed to read data from socket");
+
+                        if n == 0 {
+                            return;
+                        }
+                        println!("{:?}", buf);
+                    }
+                });
+            }
+        }));
         while self.running {
+            self.tick += 1;
             let received = tr.recv().await.unwrap();
             match received {
                 Receivable::Command(command) => {
